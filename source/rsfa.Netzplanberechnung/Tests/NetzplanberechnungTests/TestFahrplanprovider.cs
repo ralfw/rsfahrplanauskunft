85ce143a1a5de7717ba62435abc25518cd7c96bc
tenders <Thomas.Enders@gmx.de>
2014-02-07 15:06:28 +0100

    Added c'tor and tests

---
+++ source/rsfa.Netzplanberechnung/Tests/NetzplanberechnungTests/TestFahrplanprovider.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetzplanberechnungTests
+{
+    using rsfa.contracts;
+
+    public class TestFahrplanprovider : IFahrplanProvider
+    {
+        private Dictionary<string, string[]> linienHaltestellen = new Dictionary<string, string[]>(); 
+        
+        public DateTime[] Abfahrtszeiten_bei_Haltestelle(string linienname, string haltestellenname)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TimeSpan[] Fahrtdauern_für_Linie(string linienname)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] Haltestellen_für_Linie(string linienname)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] Liniennamen
+        {
+            get
+            {
+                return this.linienHaltestellen.Keys.ToArray();
+            }
+        }
+
+        private void InitHaltestellen()
+        {
+            this.linienHaltestellen.Add("S1Ost", new string[] { "Stachus", "Marienplatz", "Isartor", "Rosenheimer"});
+            this.linienHaltestellen.Add("S1Ost", new string[] { "Rosenheimer", "Isartor", "Marienplatz", "Stachus" });
+            this.linienHaltestellen.Add("S2", new string[] { "Stachus", "Marienplatz", "Isartor", "Rosenheimer" });
+        }
+    }
+}
