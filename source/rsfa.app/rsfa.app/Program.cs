5a6f0c75ba3c8c5767cb5a316ef54713e4098dda
Ralf Westphal <info@ralfw.de>
2014-02-07 14:58:15 +0100

    Verbindungsbewertung skellett; app integriert

--- source/rsfa.app/rsfa.app/Program.cs
+++ source/rsfa.app/rsfa.app/Program.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using rsfa.verbindungsbewertung;
 
 namespace rsfa.app
 {
@@ -9,7 +10,21 @@ namespace rsfa.app
     {
         static void Main(string[] args)
         {
-            
+            var kommando = new Kommandozeilenportal(args);
+            var konsole = new Konsolenprovider();
+
+            var fpprov = new FahrplanProvider.FahrplanProvider();
+            var netz = new Netzplanberechnung.Netzplanberechnung();
+            var pfade = new pfadbestimmung.Pfadbestimmung();
+            var verb = new VerbindungsErzeugung.VerbindungsErzeugung();
+            var bewert = new Verbindungsbewertung();
+
+            pfade.OnPfad += pfad => verb.Verbindugen_zu_Pfad_bilden(pfad, kommando.Startzeit);
+            verb.OnVerbindung += bewert.Verbindungen_bewerten;
+            bewert.OnVerbindungenKomplett += konsole.Verbindungen_anzeigen;
+
+            var netzplan = netz.Netzplan_berechnen();
+            pfade.Alle_Pfade_bestimmen(netzplan, kommando.Starthaltestellenname, kommando.Zielhaltestellenname);
         }
     }
 }
