115b1e5efb122ce895d827ede1ed0b8664c904c5
ruehland <ste.rue@web.de>
2014-02-07 14:10:48 +0100

    first draft

---
+++ source/rsfa.FahrplanProvider/rsfa.FahrplanProvider/FahrplanProvider.cs
@@ -0,0 +1,36 @@
+﻿using rsfa.contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rsfa.FahrplanProvider
+{
+    public class FahrplanProvider : IFahrplanProvider
+    {
+
+        public DateTime[] Abfahrtszeiten_bei_Haltestelle(string linienname, string haltestellenname)
+        {
+            return new  DateTime[3];
+        }
+
+        public TimeSpan[] Fahrtdauern_für_Linie(string linienname)
+        {
+            return new TimeSpan[3];
+        }
+
+        public string[] Haltestellen_für_Linie(string linienname)
+        {
+            return new string[3];
+        }
+
+        public string[] Liniennamen
+        {
+            get
+            {
+                return new string[3];
+            }
+        }
+    }
+}
