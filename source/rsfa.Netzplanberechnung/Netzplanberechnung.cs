a271ecaccb351818f03d127a1889a688936cc529
tenders <Thomas.Enders@gmx.de>
2014-02-07 16:31:39 +0100

    new

--- source/rsfa.Netzplanberechnung/Netzplanberechnung.cs
+++ source/rsfa.Netzplanberechnung/Netzplanberechnung.cs
@@ -6,14 +6,70 @@ using System.Threading.Tasks;
 using rsfa.contracts;
 using rsfa.contracts.daten;
 
-namespace Netzplanberechnung
+namespace rsfa.Netzplanberechnung
 {
     public class Netzplanberechnung : INetzplanberechnung
     {
+        private IFahrplanProvider fahrplanprovider;
+
+        public Netzplanberechnung(IFahrplanProvider fahrplanProvider)
+        {
+            this.fahrplanprovider = fahrplanProvider;
+        }
+
         public Netzplan Netzplan_berechnen()
         {
-            // Hallo3
-            return new Netzplan();
+            var netzplan = new Netzplan { Haltestellen = this.FülleHalteStellen().ToArray() };
+            return netzplan;
+        }
+
+        private IEnumerable<Haltestelle> FülleHalteStellen()
+        {
+            var alleHalteStellen = this.AlleHaltestellen();
+            foreach (var linienName in this.fahrplanprovider.Liniennamen)
+            {
+                var haltestellenFürLinie = this.fahrplanprovider.Haltestellen_für_Linie(linienName);
+                for (int index = 1; index < haltestellenFürLinie.Length; index++)
+                {
+                    var haltestellenName = haltestellenFürLinie[index - 1];
+                    var zielHaltestellenName = haltestellenFürLinie[index];
+                    var halteStelle = alleHalteStellen[haltestellenName];
+                    var zielHalteStelle = alleHalteStellen[zielHaltestellenName];
+                    this.Strecke_Hinzufügen(halteStelle, linienName, zielHalteStelle);
+                }
+            }
+
+            return alleHalteStellen.Values;
+        }
+
+        private Dictionary<string, Haltestelle> AlleHaltestellen()
+        {
+            var halteStellen = new Dictionary<string, Haltestelle>();
+            foreach (var linienName in this.fahrplanprovider.Liniennamen)
+            {
+                foreach (var haltestellenName in this.fahrplanprovider.Haltestellen_für_Linie(linienName)
+                    .Where(haltestellenName => !halteStellen.ContainsKey(haltestellenName)))
+                {
+                    halteStellen[haltestellenName] = new Haltestelle
+                                                         {
+                                                             Strecken = new Strecke[] { }, 
+                                                             Name = haltestellenName
+                                                         };
+                }
+            }
+
+            return halteStellen;
+        }
+
+        private void Strecke_Hinzufügen(Haltestelle halteStelle, string linienname, Haltestelle ziel)
+        {
+            var strecke = new Strecke();
+            strecke.Linienname = linienname;
+            strecke.Zielhaltestellenname = ziel.Name;
+            
+            var aktuelleStrecken = halteStelle.Strecken.ToList();
+            aktuelleStrecken.Add(strecke);
+            halteStelle.Strecken = aktuelleStrecken.ToArray();
         }
     }
 }
--- source/rsfa.Netzplanberechnung/Tests/NetzplanberechnungTests/NetzplanberechnungTest.cs
+++ source/rsfa.Netzplanberechnung/Tests/NetzplanberechnungTests/NetzplanberechnungTest.cs
@@ -1,14 +1,66 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace NetzplanberechnungTests
 {
+    using rsfa.Netzplanberechnung;
+    using rsfa.contracts.daten;
+
     [TestClass]
     public class NetzplanberechnungTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Netzplan_berechnenCtorTest()
         {
+            TestFahrplanprovider testProvider = new TestFahrplanprovider();
+            Assert.IsNotNull(testProvider);
+            Netzplanberechnung berechnung = new Netzplanberechnung(testProvider);
+            Assert.IsNotNull(berechnung);
+        }
+
+        [TestMethod]
+        public void Netzplan_berechnenTest()
+        {
+            var plan = instantiateNetzplanBerechner();
+            Assert.IsNotNull(plan);
+
+            var halteMarien = plan.Haltestellen.Single(hs => hs.Name.Contains("Marienplatz"));
+            Assert.IsTrue(halteMarien.Strecken.Length == 4);
+            Assert.IsTrue(halteMarien.Strecken.Count(s => s.Linienname == "S2" && s.Zielhaltestellenname == "Isartor") == 1);
+            Assert.IsTrue(halteMarien.Strecken.Count(s => s.Linienname == "S1OOst" && s.Zielhaltestellenname == "Isartor") == 1);
+            Assert.IsTrue(halteMarien.Strecken.Count(s => s.Linienname == "S1OWest" && s.Zielhaltestellenname == "Stachus") == 1);
+            Assert.IsTrue(halteMarien.Strecken.Count(s => s.Linienname == "S1OWest" && s.Zielhaltestellenname == "Isartor") == 0);
+            Assert.IsTrue(halteMarien.Strecken.Count(s => s.Linienname == "U3" && s.Zielhaltestellenname == "Sendlinger") == 0);
+            Assert.IsTrue(halteMarien.Strecken.Count(s => s.Linienname == "U3" && s.Zielhaltestellenname == "Odeons") == 1);
+        }
+
+        [TestMethod]
+        public void Netzplan_berechnenSendlingerTest()
+        {
+            var plan = instantiateNetzplanBerechner();
+            Assert.IsNotNull(plan);
+
+            var halteMarien = plan.Haltestellen.Single(hs => hs.Name.Contains("Sendlinger"));
+            Assert.IsTrue(halteMarien.Strecken.Length == 1);
+            Assert.IsTrue(halteMarien.Strecken.Count(s => s.Linienname == "U3" && s.Zielhaltestellenname == "Marienplatz") == 1);
+        }
+
+        [TestMethod]
+        public void Netzplan_berechnenUnbekannteHalteStelleTest()
+        {
+            var plan = instantiateNetzplanBerechner();
+            Assert.IsFalse(plan.Haltestellen.Any(hs => hs.Name.Contains("jdsfk")));
+        }
+
+
+        private static Netzplan instantiateNetzplanBerechner()
+        {
+            TestFahrplanprovider testProvider = new TestFahrplanprovider();
+
+            Netzplanberechnung berechnung = new Netzplanberechnung(testProvider);
+            var plan = berechnung.Netzplan_berechnen();
+            return plan;
         }
     }
 }
