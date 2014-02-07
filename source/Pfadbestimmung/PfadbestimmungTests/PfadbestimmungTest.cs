f923c1ef473621d963ee78b1e99f008204c9c331
sadokc <sadok.ch@gmail.com>
2014-02-07 16:31:56 +0100

    Added graphviz visualizer for `Netzplan`

--- source/Pfadbestimmung/PfadbestimmungTests/PfadbestimmungTest.cs
+++ source/Pfadbestimmung/PfadbestimmungTests/PfadbestimmungTest.cs
@@ -5,6 +5,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using rsfa.pfadbestimmung;
 using rsfa.contracts;
 using rsfa.contracts.daten;
+using System.Text;
 
 
 namespace PfadbestimmungTests
@@ -98,6 +99,39 @@ namespace PfadbestimmungTests
          }
       };
 
+      private Netzplan KomplexerNetzplan3 = new Netzplan()
+      {
+         Haltestellen = new[]
+         {
+            new Haltestelle
+            {
+               Name = "Start",
+               Strecken = new[]
+               {
+                  new Strecke { Linienname = "U1 Ost", Zielhaltestellenname = "Mitte" },
+               },
+            },
+            new Haltestelle
+            {
+               Name = "Mitte",
+               Strecken = new[]
+               {
+                  new Strecke { Linienname = "U1 Ost", Zielhaltestellenname = "Ziel" },
+                  new Strecke { Linienname = "Bus", Zielhaltestellenname = "Ziel" },
+                  new Strecke { Linienname = "U1 West", Zielhaltestellenname = "Start" },
+               },
+            },
+            new Haltestelle
+            {
+               Name = "Ziel",
+               Strecken = new[]
+               {
+                  new Strecke { Linienname = "U1 West", Zielhaltestellenname = "Mitte" },
+               },
+            },
+         }
+      };
+
       [TestMethod]
       public void EinfacherNetzplanMitEinemPfad()
       {
@@ -145,14 +179,59 @@ namespace PfadbestimmungTests
 
          Int32 gefundenePfade = 0;
          target.OnPfad += (pfad) =>
+         {
+            gefundenePfade++;
+            Assert.AreEqual(ziel, pfad.Strecken.Last().Zielhaltestellenname, "Ziel stimmt nicht");
+            Assert.AreEqual(start, pfad.Starthaltestellenname, "Start stimmt nicht");
+         };
+
+         target.Alle_Pfade_bestimmen(this.KomplexerNetzplan2, start, ziel);
+         Assert.AreEqual(2, gefundenePfade, "Anzahl Pfade passt nicht");
+      }
+
+      [TestMethod]
+      public void KomplexerNetzplanMitZwoaPfaden3()
+      {
+         var target = new Pfadbestimmung();
+         var start = "Start";
+         var ziel = "Ziel";
+
+         Int32 gefundenePfade = 0;
+         target.OnPfad += (pfad) =>
             {
                gefundenePfade++;
                Assert.AreEqual(ziel, pfad.Strecken.Last().Zielhaltestellenname, "Ziel stimmt nicht");
                Assert.AreEqual(start, pfad.Starthaltestellenname, "Start stimmt nicht");
             };
 
-         target.Alle_Pfade_bestimmen(this.KomplexerNetzplan2, start, ziel);
+         target.Alle_Pfade_bestimmen(this.KomplexerNetzplan3, start, ziel);
          Assert.AreEqual(2, gefundenePfade, "Anzahl Pfade passt nicht");
+      }
+
+      [TestMethod]
+      public void ZeichneNetzpläne()
+      {
+         // cut and paste strings to http://graphviz-dev.appspot.com/
+         var dot1 = this.GenerateDot(this.EinfachsterNetzplan);
+         var dot3 = this.GenerateDot(this.KomplexerNetzplan3);
+      }
+
+      private String GenerateDot(Netzplan netzplan)
+      {
+         var sb = new StringBuilder()
+            .AppendLine("digraph Netzplan {");
+
+         foreach (var haltestelle in netzplan.Haltestellen)
+         {
+            foreach (var strecke in haltestelle.Strecken)
+            {
+               sb.AppendFormat("  \"{0}\" -> \"{1}\" [label=\"{2}\"];", haltestelle.Name, strecke.Zielhaltestellenname, strecke.Linienname);
+               sb.AppendLine();
+            }
+         }
+
+         sb.AppendLine("}");
+         return sb.ToString();
       }
    }
 }
