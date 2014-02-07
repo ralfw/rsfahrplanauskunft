ce7e39ce2974e2da6152e4c3f43ceee2a4fa329e
sadokc <sadok.ch@gmail.com>
2014-02-07 14:08:14 +0100

    Version 0: Projektstuktur

---
+++ source/Pfadbestimmung/Pfadbestimmung/Pfadbestimmung.cs
@@ -0,0 +1,23 @@
+﻿using rsfa.contracts;
+using rsfa.contracts.daten;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rsfa.pfadbestimmung
+{
+   public class Pfadbestimmung : IPfadbestimmung
+   {
+      public void Alle_Pfade_bestimmen(Netzplan netzplan, string starthaltestellenname, string zielhaltestellenname)
+      {
+         if (this.OnPfad != null)
+         {
+            this.OnPfad(new Pfad());
+         }
+      }
+
+      public event Action<Pfad> OnPfad;
+   }
+}
