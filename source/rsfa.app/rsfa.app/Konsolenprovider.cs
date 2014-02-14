a825e11d8e945cae01f132a340746e21a38fbc25
mmkammerer <mmkammerer@web.de>
2014-02-14 12:06:32 +0100

    Bessere Ausgabe im Konsolenprovider

--- source/rsfa.app/rsfa.app/Konsolenprovider.cs
+++ source/rsfa.app/rsfa.app/Konsolenprovider.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using rsfa.contracts.daten;
 
 namespace rsfa.app
@@ -8,9 +9,35 @@ namespace rsfa.app
     {
         public void Verbindungen_anzeigen(IEnumerable<Verbindung> verbindungen)
         {
+            bool headerShown = false;
+
             foreach (var v in verbindungen)
             {
-                Console.WriteLine("Verb: {0}", v.Pfad.Starthaltestellenname);
+                int anzahlStrecken = v.Pfad.Strecken.Length;
+
+                if (!headerShown)
+                {
+                    Console.WriteLine(
+                        "Verbindungen von {0} nach {1}",
+                        v.Pfad.Starthaltestellenname,
+                        v.Pfad.Strecken.Last().Zielhaltestellenname);
+                    headerShown = true;
+                }
+
+                Console.WriteLine(
+                    "Abfahrt {0}, Ankunft {1}, Reisezeit {2}",
+                    v.Fahrtzeiten.First().Abfahrtszeit,
+                    v.Fahrtzeiten.Last().Ankunftszeit,
+                    v.Fahrtzeiten.Last().Ankunftszeit - v.Fahrtzeiten.First().Abfahrtszeit);
+
+                for (int i = 0; i < anzahlStrecken; i++)
+                {
+                    Console.WriteLine(
+                        "  mit {0} bis {1}",
+                        v.Pfad.Strecken[i].Linienname,
+                        v.Pfad.Strecken[i].Zielhaltestellenname);
+                }
+
             }
         }
     }
