e6f8212c7e40e10e1f450a8c8348f810ebf099f8
Pascal Wolff <pascal.wolff@gmail.com>
2014-02-14 09:47:12 +0100

    Eigene Klasse für Visualisierer

---
+++ source/rsfa.app/rsfa.app/NetzplanVisualisierer.cs
@@ -0,0 +1,34 @@
+﻿namespace rsfa.app
+{
+    using System;
+    using System.IO;
+    using System.Text;
+
+    using rsfa.contracts.daten;
+
+    public class NetzplanVisualisierer
+    {
+        public void SchreibeDotFile(Netzplan netzplan)
+        {
+            File.WriteAllLines(@"C:\tmp\dot1.txt", new[] { this.GenerateDot(netzplan) });
+        }
+
+        private String GenerateDot(Netzplan netzplan)
+        {
+            var sb = new StringBuilder()
+                .AppendLine("digraph Netzplan {");
+
+            foreach (var haltestelle in netzplan.Haltestellen)
+            {
+                foreach (var strecke in haltestelle.Strecken)
+                {
+                    sb.AppendFormat("  \"{0}\" -> \"{1}\" [label=\"{2}\"];", haltestelle.Name, strecke.Zielhaltestellenname, strecke.Linienname);
+                    sb.AppendLine();
+                }
+            }
+
+            sb.AppendLine("}");
+            return sb.ToString();
+        }
+    }
+}
